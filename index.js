DevinClark/Angular-Rome

commit 4ac8dd35c9e05792f6d34194e748b25841dee740

Fix ng-change
TODO: figure out why it runs twice. Probably has to do with formatting.

index.js
@@ -67,7 +67,8 @@ rome_module.directive('rome', function romeDirective(romeDefaults, $interval) {
     restrict: 'AE',
     transclude: 'attributes',
     scope: {
-      ngModel: '='
+      ngModel: '=',
+      ngChange: '=?'
     },
     require: '^ngModel',
     template: '<div class="rome-container">' +
@@ -119,7 +120,8 @@ rome_module.directive('rome', function romeDirective(romeDefaults, $interval) {
         'novalidate': attrs.novalidate,
         'ng-value': attrs.ngValue,
         'ng-disabled': attrs.ngDisabled,
-        'ng-change': attrs.ngChange
+        'ng-change': attrs.ngChange,
+        'ng-model': attrs.ngModel
       }, function (value, name) {
         if (angular.isDefined(value)) {
           el.removeAttr(name);

commit 8070c2ce10e544b5a66a6bb8b5706691884cd36d

Fix range validation regression caused by 4ac8dd35c9e.

index.js
@@ -33,13 +33,14 @@ rome_module.directive('rome', function romeDirective(romeDefaults, $interval) {
       var has_id = romeValidator.indexOf('#') === 0;
       var matched_element;
       var search_attr;
-      var rome_elements = document.getElementsByTagName('rome');
+      var rome_elements = angular.element(document.getElementsByTagName('rome')).find('input');
+      var search_element;
       if (has_id) {
-        matched_element = angular.element(document.getElementById(romeValidator.substr(1))).find('input');
+        matched_element = angular.element(document.getElementById(romeValidator.substr(1)));
       } else {
         for (var i = 0; i < rome_elements.length; i++) {
           if (rome_elements[i].getAttribute('ng-model') == romeValidator) {
-            matched_element = angular.element(rome_elements[i]).find('input');
+            matched_element = angular.element(rome_elements[i]);
             break;
           }
         }